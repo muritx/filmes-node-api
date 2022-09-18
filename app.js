65671bff2cffba33b2995585a944d9578ea246b3
Murilo Teixeira <murilosantosx@hotmail.com>
2022-09-18 17:10:49 -0300

    Update

--- app.js
+++ app.js
@@ -59,4 +59,4 @@ app.delete('/movie/:id', (req,res) => {
     res.send("FIlme foi apagado!");
 });
 
-app.listen(port, () => console.log('Servidor iniciado na porta 8080'));
+app.listen(port, () => console.log('Servidor iniciado na porta ${port}'));
